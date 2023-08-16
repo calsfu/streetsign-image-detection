f0da83e59aa5f0be0b1c2afd97c24b32c2691a3a
calsfu <colerezi30@gmail.com>
2023-08-15 19:31:12 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,121 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda, Compose
+import matplotlib.pyplot as plt
+
+# Download training data from open datasets.
+training_data = datasets.GTSRB(
+    root="data",
+    split="train",
+    download=True,
+    transform=ToTensor(),
+)
+
+test_data = datasets.GTSRB(
+    root="data",
+    split="test",
+    download=True,
+    transform=ToTensor(),
+)
+
+batch_size = 1
+
+# Create data loaders.
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+print("Training data: ", test_dataloader)
+
+
+# Check size of data
+for X, y in train_dataloader:
+    print("Shape of X [N, C, H, W]: ", X.shape)
+    print("Shape of y: ", y.shape, y.dtype)
+    break
+
+#display sample data
+figure = plt.figure(figsize=(10, 8))
+cols, rows = 5, 5
+for i in range(1, cols * rows + 1):
+    idx = torch.randint(len(test_data), size=(1,)).item()
+    img, label = test_data[idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(label)
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(device))
+
+# Define model
+# class NeuralNetwork(nn.Module):
+#     def __init__(self):
+#         super(NeuralNetwork, self).__init__()
+#         self.flatten = nn.Flatten()
+#         self.linear_relu_stack = nn.Sequential(
+#             nn.Linear(28*28, 512),
+#             nn.ReLU(),
+#             nn.Linear(512, 512),
+#             nn.ReLU(),
+#             nn.Linear(512, 10),
+#             nn.ReLU()
+#         )
+#     def forward(self, x):
+#         x = self.flatten(x)
+#         logits = self.linear_relu_stack(x)
+#         return logits
+    
+# model = NeuralNetwork().to(device)
+# print(model)
+
+# loss_fn = nn.CrossEntropyLoss()
+# learning_rate = 1e-3
+# optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# def train(dataloader, model, loss_fn, optimizer):
+#     size = len(dataloader.dataset)
+#     for batch, (X, y) in enumerate(dataloader):
+#         X, y = X.to(device), y.to(device)
+        
+#         # Compute prediction error
+#         pred = model(X)
+#         loss = loss_fn(pred, y)
+        
+#         # Backpropagation
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+
+#         if batch % 100 == 0:
+#             loss, current = loss.item(), batch * len(X)
+#             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+# def test(dataloader, model):
+#     size = len(dataloader.dataset)
+#     model.eval()
+#     test_loss, correct = 0, 0
+#     with torch.no_grad():
+#         for X, y in dataloader:
+#             X, y = X.to(device), y.to(device)
+#             pred = model(X)
+#             test_loss += loss_fn(pred, y).item()
+#             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+#     test_loss /= size
+#     correct /= size
+#     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+# epochs = 15
+# for t in range(epochs):
+#     print(f"Epoch {t+1}\n-------------------------------")
+#     train(train_dataloader, model, loss_fn, optimizer)
+#     test(test_dataloader, model)
+# print("Done!")
+
+# torch.save(model.state_dict(), "data/model.pth")
+# print("Saved PyTorch Model State to model.pth")
+
+# model = NeuralNetwork()
+# model.load_state_dict(torch.load("data/model.pth"))
