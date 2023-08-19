eb898a33a443a07b1c9ef934d78594b8cfb91ae2
calsfu <colerezi30@gmail.com>
2023-08-18 17:55:06 -0700

    Added labels

--- main.py
+++ main.py
@@ -12,6 +12,52 @@ import matplotlib.pyplot as plt
 os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 os.environ["TORCH_USE_CUDA_DSA"] = "1"
 
+labels = {
+    0: "Speed limit (20km/h)",
+    1: "Speed limit (30km/h)",
+    2: "Speed limit (50km/h)",
+    3: "Speed limit (60km/h)",
+    4: "Speed limit (70km/h)",
+    5: "Speed limit (80km/h)",
+    6: "End of speed limit (80km/h)",
+    7: "Speed limit (100km/h)",
+    8: "Speed limit (120km/h)",
+    9: "No passing",
+    10: "No passing veh over 3.5 tons",
+    11: "Right-of-way at intersection",
+    12: "Priority road",
+    13: "Yield",
+    14: "Stop",
+    15: "No vehicles",
+    16: "Veh > 3.5 tons prohibited",
+    17: "No entry",
+    18: "General caution",
+    19: "Dangerous curve left",
+    20: "Dangerous curve right",
+    21: "Double curve",
+    22: "Bumpy road",
+    23: "Slippery road",
+    24: "Road narrows on the right",
+    25: "Road work",
+    26: "Traffic signals",
+    27: "Pedestrians",
+    28: "Children crossing",
+    29: "Bicycles crossing",
+    30: "Beware of ice/snow",
+    31: "Wild animals crossing",
+    32: "End speed + passing limits",
+    33: "Turn right ahead",
+    34: "Turn left ahead",
+    35: "Ahead only",
+    36: "Go straight or right",
+    37: "Go straight or left",
+    38: "Keep right",
+    39: "Keep left",
+    40: "Roundabout mandatory",
+    41: "End of no passing",
+    42: "End no passing veh > 3.5 tons"
+}
+
 desired_size = (28, 28)
 
 #resizes images to 32x32 since images are different sizes
@@ -39,17 +85,17 @@ test_data = datasets.GTSRB(
 
 
 
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 
 figure = plt.figure(figsize=(20, 20))
-cols, rows = 16, 16
+cols, rows = 17, 17
 
 # for image_path, class_label in training_data._samples:
 #     # Extract the class label from the folder name in the image_path
     
 #     print(f"Image: {image_path}, Class Label: {class_label}")
 
-for i in range(1, 217):
+for i in range(1, 222):
     
     img = training_data[i*120][0][1] #gets image
     label = training_data.__getitem__(i*120)[1] #gets labek
